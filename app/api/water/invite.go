74c12e60d331ca290fb076a3274d6d6fdc30f5c2
抖音牌轻薄本 <a15315862177@163.com>
2021-10-30 22:15:48 +0800

    commit

--- app/api/water/invite.go
+++ app/api/water/invite.go
@@ -3,6 +3,7 @@ package api
 import (
 	"sea/app/service"
 
+	"github.com/ProtonMail/gopenpgp/v2/crypto"
 	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/net/ghttp"
 )
@@ -15,13 +16,13 @@ var WaterInvite = waterInviteApi{}
 type waterInviteApi struct{}
 
 type WaterInviteStep1Req struct {
-	SenderPublicKey string `json:"sender"`
-	Session         string `json:"session"`
+	SenderPublicKey string `json:"sender"` // a 4096 bits OpenPGP public key
 }
 
 type WaterInviteStep1Resp struct {
+	Session                    string `json:"session"`  // a 64 character random string
+	EncryptedReceiverPublicKey string `json:"receiver"` // a encrypted OpenPGP public key
 	ReturnCode                 int    `json:"returnCode"`
-	EncryptedReceiverPublicKey string `json:"receiver"`
 }
 
 type WaterInviteStep2Req struct {
@@ -34,12 +35,12 @@ type WaterInviteStep2Resp struct {
 }
 
 const (
-	INVITE_RETURN_CODE_SUCCESS           = 0
-	INVITE_RETURN_CODE_DECRYPTION_FAILED = 1
-	INVITE_RETURN_CODE_SESSION_NOT_FOUND = 2
-	INVITE_RETURN_CODE_BAD_KEY           = 3
-	INVITE_RETURN_CODE_BAD_RANDOM_STRING = 4
-	INVITE_RETURN_CODE_ALREADY_EXISTS    = 5
+	INVITE_RETURN_CODE_SUCCESS            = 0
+	INVITE_RETURN_CODE_DECRYPTION_FAILED  = 1
+	INVITE_RETURN_CODE_SESSION_NOT_FOUND  = 2
+	INVITE_RETURN_CODE_BAD_KEY            = 3
+	INVITE_RETURN_CODE_BAD_RANDOM_STRING  = 4
+	INVITE_RETURN_CODE_KEY_ALREADY_EXISTS = 5
 )
 
 func WaterInviteApiMiddleware(r *ghttp.Request) {
@@ -50,8 +51,17 @@ func WaterInviteApiMiddleware(r *ghttp.Request) {
 func (*waterInviteApi) Step1(r *ghttp.Request) {
 	var req *WaterInviteStep1Req
 	r.Parse(req)
+	// verify the public key
+	k, err := crypto.NewKeyFromArmored(req.SenderPublicKey)
+	if (err != nil) || (!k.CanVerify()) || (!k.IsPrivate()) || (!k.IsExpired()) {
+		r.Response.WriteJson(WaterInviteStep1Resp{
+			ReturnCode: INVITE_RETURN_CODE_BAD_KEY,
+		})
+		return
+	}
+	// save the public key to database
 
-	// w, _ := service.Water.GetSelfWater()
+	// generate a session
 
 	r.Response.WriteJson(WaterInviteStep1Resp{})
 }
