8b0df319d08143a5a2c9d96f6ea3e73090836deb
sbchild <a15315862177@163.com>
2021-11-06 10:15:54 +0800

    rewrite

--- app/api/water/invite.go
+++ app/api/water/invite.go
@@ -1,11 +1,13 @@
 package api
 
 import (
+	// "context"
 	"sea/app/service"
-	serviceWater "sea/app/service/water"
+	// serviceWater "sea/app/service/water"
 
-	"github.com/ProtonMail/gopenpgp/v2/crypto"
-	"github.com/ProtonMail/gopenpgp/v2/helper"
+	// "github.com/ProtonMail/gopenpgp/v2/crypto"
+	// "github.com/ProtonMail/gopenpgp/v2/helper"
+	// "github.com/gogf/gf/database/gdb"
 	"github.com/gogf/gf/encoding/gparser"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
@@ -46,17 +48,6 @@ type WaterInviteStep2Pack struct {
 	RandomString string `json:"random"` // a 32 character random string
 }
 
-const (
-	INVITE_RETURN_CODE_SUCCESS            = 0 // success
-	INVITE_RETURN_CODE_DECRYPTION_FAILED  = 1 // failed to decrypt
-	INVITE_RETURN_CODE_SESSION_NOT_FOUND  = 2 // session not found
-	INVITE_RETURN_CODE_SESSION_ERROR      = 3 // can't create session, needs a retry
-	INVITE_RETURN_CODE_BAD_KEY            = 4 // invalid key, expired, a private key, banned key or empty string
-	INVITE_RETURN_CODE_BAD_RANDOM_STRING  = 5 // random string is not 32 characters long
-	INVITE_RETURN_CODE_KEY_ALREADY_EXISTS = 6 // this key already exists, return it after a successful authentication
-	INVITE_RETURN_CODE_SERVER_ERROR       = 7 // server isn't ready
-)
-
 func WaterInviteApiMiddleware(r *ghttp.Request) {
 	r.Middleware.Next()
 
@@ -67,49 +58,57 @@ func (*waterInviteApi) MakeStep1Pack(session, key string) string {
 	return r.MustToJsonString()
 }
 
-func (api *waterInviteApi) Step1(r *ghttp.Request) {
-	var req *WaterInviteStep1Req
-	r.Parse(req)
-	throw := func(code int) {
-		r.Response.WriteJsonExit(WaterInviteStep1Resp{
-			ReturnCode: code,
-		})
-	}
-	// verify the public key
-	k, err := crypto.NewKeyFromArmored(req.SenderPublicKey)
-	ks, _ := k.Armor()
-	if (err != nil) || (serviceWater.WaterKey.CheckKey(ks, false) != serviceWater.WATER_KEY_CHECK_OK) {
-		throw(INVITE_RETURN_CODE_BAD_KEY)
-	}
-	if ks := serviceWater.WaterKey.GetKeyStatus(ks); ks != serviceWater.WATER_KEY_STATUS_NOT_FOUND {
-		throw(INVITE_RETURN_CODE_BAD_KEY)
-	}
-	// create a session
-	session, err := serviceWater.WaterInvite.CreateSession()
-	if err != nil {
-		throw(INVITE_RETURN_CODE_SESSION_ERROR)
-	}
-	// save the public key and the session to database
-	err = serviceWater.WaterInvite.SetSessionSender(session, ks)
-	if err != nil {
-		throw(INVITE_RETURN_CODE_SESSION_NOT_FOUND)
-	}
-	// encrypt receiver's public key and session
-	selfKeyID, err := serviceWater.WaterKey.GetSelfKeyID()
-	if err != nil {
-		throw(INVITE_RETURN_CODE_SERVER_ERROR)
-	}
-	selfKey, _ := serviceWater.WaterKey.GetKey(selfKeyID) // this key has already been checked, so it's safe to continue
-	es, err := helper.EncryptMessageArmored(selfKey, api.MakeStep1Pack(session, selfKey))
-	if err != nil {
-		throw(INVITE_RETURN_CODE_SERVER_ERROR)
-	}
-	// fill the response
-	r.Response.WriteJsonExit(WaterInviteStep1Resp{
-		EncryptedReceiverPublicKey: es,
-		ReturnCode:                 INVITE_RETURN_CODE_SUCCESS,
-	})
-}
+// func (api *waterInviteApi) Step1(r *ghttp.Request) {
+
+// 	api.step1(r)
+// }
+
+// func (api *waterInviteApi) step1(ctx context.Context, tx *gdb.TX, r *ghttp.Request) {
+// 	var req *WaterInviteStep1Req
+// 	r.Parse(req)
+// 	// start a transaction
+
+// 	// throw func
+// 	throw := func(code int) {
+// 		r.Response.WriteJsonExit(WaterInviteStep1Resp{
+// 			ReturnCode: code,
+// 		})
+// 	}
+// 	// verify the public key
+// 	k, err := crypto.NewKeyFromArmored(req.SenderPublicKey)
+// 	ks, _ := k.Armor()
+// 	if (err != nil) || (serviceWater.WaterKey.CheckKey(ks, false) != serviceWater.WATER_KEY_CHECK_OK) {
+// 		throw(INVITE_RETURN_CODE_BAD_KEY)
+// 	}
+// 	if ks := serviceWater.WaterKey.GetKeyStatus(ks); ks != serviceWater.WATER_KEY_STATUS_NOT_FOUND {
+// 		throw(INVITE_RETURN_CODE_BAD_KEY)
+// 	}
+// 	// create a session
+// 	session, err := serviceWater.WaterInvite.CreateSession()
+// 	if err != nil {
+// 		throw(INVITE_RETURN_CODE_SESSION_ERROR)
+// 	}
+// 	// save the public key and the session to database
+// 	err = serviceWater.WaterInvite.SetSessionSender(session, ks)
+// 	if err != nil {
+// 		throw(INVITE_RETURN_CODE_SESSION_NOT_FOUND)
+// 	}
+// 	// encrypt receiver's public key and session
+// 	selfKeyID, err := serviceWater.WaterKey.GetSelfKeyID()
+// 	if err != nil {
+// 		throw(INVITE_RETURN_CODE_SERVER_ERROR)
+// 	}
+// 	selfKey, _ := serviceWater.WaterKey.GetKey(selfKeyID) // this key has already been checked, so it's safe to continue
+// 	es, err := helper.EncryptMessageArmored(selfKey, api.MakeStep1Pack(session, selfKey))
+// 	if err != nil {
+// 		throw(INVITE_RETURN_CODE_SERVER_ERROR)
+// 	}
+// 	// fill the response
+// 	r.Response.WriteJsonExit(WaterInviteStep1Resp{
+// 		EncryptedReceiverPublicKey: es,
+// 		ReturnCode:                 INVITE_RETURN_CODE_SUCCESS,
+// 	})
+// }
 
 func (*waterInviteApi) Step2(r *ghttp.Request) {
 	var req *WaterInviteStep2Req
--- app/service/water/invite.go
+++ app/service/water/invite.go
@@ -4,54 +4,73 @@ import (
 	"context"
 	"sea/app/dao"
 	"sea/app/model"
-
+	"github.com/gogf/gf/database/gdb"
 	"github.com/gogf/gf/v2/os/gtime"
 	"github.com/gogf/gf/v2/util/grand"
 )
 
-var ctx = context.Background()
+var WaterInvite = waterInviteService{}
 
-var WaterInvite = waterInviteService{
-	ctx: &ctx,
-}
+type waterInviteService struct {}
 
-type waterInviteService struct {
-	ctx *context.Context
-}
+const (
+	INVITE_RETURN_CODE_SUCCESS            = 0 // success
+	INVITE_RETURN_CODE_DECRYPTION_FAILED  = 1 // failed to decrypt
+	INVITE_RETURN_CODE_SESSION_NOT_FOUND  = 2 // session not found
+	INVITE_RETURN_CODE_SESSION_ERROR      = 3 // can't create session, needs a retry
+	INVITE_RETURN_CODE_BAD_KEY            = 4 // invalid key, expired, a private key, banned key or empty string
+	INVITE_RETURN_CODE_BAD_RANDOM_STRING  = 5 // random string is not 32 characters long
+	INVITE_RETURN_CODE_KEY_ALREADY_EXISTS = 6 // this key already exists, return it after a successful authentication
+	INVITE_RETURN_CODE_SERVER_ERROR       = 7 // server isn't ready
+)
 
-func (s *waterInviteService) CreateSession() (string, error) {
-	sessionId := grand.S(64, true)
-	_, err := dao.WaterInvite.Ctx(*s.ctx).Data(model.WaterInvite{
-		Session:         sessionId,
-		SenderPublicKey: "",
-	}).Insert()
-	return sessionId, err
-}
-
-func (s *waterInviteService) SetSessionSender(sessionId string, publicKey string) error {
-	_, err := dao.WaterInvite.Ctx(*s.ctx).Where(model.WaterInvite{Session: sessionId}).Data(model.WaterInvite{SenderPublicKey: publicKey}).Update()
-	return err
-}
-
-func (s *waterInviteService) GetSessionSender(sessionId string) (string, error) {
-	var m *model.WaterInvite
-	err := dao.WaterInvite.Ctx(*s.ctx).Where(model.WaterInvite{Session: sessionId}).Scan(&m)
-	if err != nil {
-		return "", err
+func (s *waterInviteService) InviteStep1(senderPublicKey string) (EncryptedReceiverPublicKey string, ReturnCode int) {
+	wrap := func(ctx context.Context, tx *gdb.TX) error {
+		EncryptedReceiverPublicKey, ReturnCode = s.inviteStep1(ctx, tx, senderPublicKey)
+		return nil
 	}
-	return m.Session, nil
+	dao.Water.Transaction(context.TODO(), func(ctx context.Context, tx *gdb.TX) error {
+		return wrap(ctx, tx)
+	})
+	return
+}
+func (s *waterInviteService) inviteStep1(ctx context.Context, tx *gdb.TX, senderPublicKey string) (string, int) {
+	return "", INVITE_RETURN_CODE_SUCCESS
 }
 
-func (s *waterInviteService) GetSessionCreateTime(sessionId string) (*gtime.Time, error) {
-	var m *model.WaterInvite
-	err := dao.WaterInvite.Ctx(*s.ctx).Where(model.WaterInvite{Session: sessionId}).Scan(&m)
-	if err != nil {
-		return gtime.New(), err
-	}
-	return m.CreatedAt, nil
-}
+// func (s *waterInviteService) CreateSession() (string, error) {
+// 	sessionId := grand.S(64, true)
+// 	_, err := dao.WaterInvite.Ctx(*s.ctx).Data(model.WaterInvite{
+// 		Session:         sessionId,
+// 		SenderPublicKey: "",
+// 	}).Insert()
+// 	return sessionId, err
+// }
 
-func (s *waterInviteService) DeleteSession(sessionId string) error {
-	_, err := dao.WaterInvite.Ctx(*s.ctx).Where(model.WaterInvite{Session: sessionId}).Delete()
-	return err
-}
+// func (s *waterInviteService) SetSessionSender(sessionId string, publicKey string) error {
+// 	_, err := dao.WaterInvite.Ctx(*s.ctx).Where(model.WaterInvite{Session: sessionId}).Data(model.WaterInvite{SenderPublicKey: publicKey}).Update()
+// 	return err
+// }
+
+// func (s *waterInviteService) GetSessionSender(sessionId string) (string, error) {
+// 	var m *model.WaterInvite
+// 	err := dao.WaterInvite.Ctx(*s.ctx).Where(model.WaterInvite{Session: sessionId}).Scan(&m)
+// 	if err != nil {
+// 		return "", err
+// 	}
+// 	return m.Session, nil
+// }
+
+// func (s *waterInviteService) GetSessionCreateTime(sessionId string) (*gtime.Time, error) {
+// 	var m *model.WaterInvite
+// 	err := dao.WaterInvite.Ctx(*s.ctx).Where(model.WaterInvite{Session: sessionId}).Scan(&m)
+// 	if err != nil {
+// 		return gtime.New(), err
+// 	}
+// 	return m.CreatedAt, nil
+// }
+
+// func (s *waterInviteService) DeleteSession(sessionId string) error {
+// 	_, err := dao.WaterInvite.Ctx(*s.ctx).Where(model.WaterInvite{Session: sessionId}).Delete()
+// 	return err
+// }
