bd8d23150e33cf070c9095674b734caba25bdfff
sbchild <a15315862177@163.com>
2022-01-07 11:11:08 +0800

    commit

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module sea
 
 go 1.17
 
-require github.com/gogf/gf/v2 v2.0.0-rc.0.20220104132444-99455e328b35
+require (
+	github.com/gogf/gf/v2 v2.0.0-rc.0.20220104132444-99455e328b35
+	github.com/lib/pq v1.10.4
+)
 
 require (
 	github.com/BurntSushi/toml v0.4.1 // indirect
@@ -25,7 +28,7 @@ require (
 	go.opentelemetry.io/otel v1.3.0 // indirect
 	go.opentelemetry.io/otel/sdk v1.3.0 // indirect
 	go.opentelemetry.io/otel/trace v1.3.0 // indirect
-	golang.org/x/net v0.0.0-20211216030914-fe4d6282115f // indirect
+	golang.org/x/net v0.0.0-20220105145211-5b0dc2dfae98 // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	golang.org/x/text v0.3.8-0.20211105212822-18b340fc7af2 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -3,12 +3,64 @@ package cmd
 import (
 	"context"
 	"sea/internal/handler"
+	serviceWater "sea/internal/service/water"
+
+	_ "github.com/lib/pq"
 
 	"github.com/gogf/gf/v2/container/gvar"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
+	"github.com/gogf/gf/v2/os/gbuild"
 	"github.com/gogf/gf/v2/os/gcmd"
 )
+
+func start(ctx context.Context, parser *gcmd.Parser) (err error) {
+	// display welcome info
+	welcome(ctx)
+	// check the key pair exists
+	g.Log().Info(ctx, "Checking the key pair ...")
+	checkKeyPair(ctx)
+	// start the web server
+	g.Log().Info(ctx, "Starting http server ...")
+	return routes(ctx, parser)
+}
+
+func welcome(ctx context.Context) {
+	g.Log().Info(ctx, "Starting the chat server - Sea ...")
+	buildInfo := gbuild.Info()
+	if buildInfo["gf"] == "" {
+		g.Log().Warning(ctx, "(debug version)")
+	} else {
+		g.Log().Infof(ctx, "built at %s in %s with gf%s", buildInfo["time"], buildInfo["go"], buildInfo["gf"])
+		g.Log().Infof(ctx, "commit: %s", buildInfo["git"])
+	}
+}
+
+func checkKeyPair(ctx context.Context) {
+	k, err := serviceWater.WaterKey.GetSelfKey(context.Background())
+	if err != nil {
+		g.Log().Warningf(ctx, "Failed to get the key pair: %s, will generate...", err.Error())
+		ks, err := serviceWater.GenerateKey()
+		if err != nil {
+			g.Log().Fatal(ctx, err)
+		}
+		kid, _ := serviceWater.GetKeyID(&ks.PublicKey)
+		g.Log().Infof(ctx, "Generated the key pair: %s", kid)
+		_, err = serviceWater.WaterKey.AddSelfKey(context.Background(), ks)
+		if err != nil {
+			g.Log().Fatal(ctx, err)
+		}
+		g.Log().Infof(ctx, "Added the key pair to database.")
+	} else {
+		g.Log().Infof(ctx, "Got the key pair: %s", k.GetKeyID())
+	}
+	k, _ = serviceWater.WaterKey.GetSelfKey(context.Background()) // refresh the key
+	if p, err := k.GetPublicKey(); err == nil {
+		g.Log().Info(ctx, "Public key: \n", p)
+	} else {
+		g.Log().Fatal(ctx, err)
+	}
+}
 
 func routes(ctx context.Context, parser *gcmd.Parser) (err error) {
 	s := g.Server()
@@ -83,6 +135,6 @@ var (
 		Name:  "sea",
 		Usage: "sea",
 		Brief: "start sea server",
-		Func:  routes,
+		Func:  start,
 	}
 )
