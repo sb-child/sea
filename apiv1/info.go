ead3974f88a00797b467dc6fe90cf7b1cadb5051
抖音牌轻薄本 <a15315862177@163.com>
2022-03-30 15:24:36 +0800

    commit

--- apiv1/info.go
+++ apiv1/info.go
@@ -11,3 +11,12 @@ type VersionRes struct {
 	g.Meta    `mime:"application/json"`
 	BuildInfo map[string]string `json:"info"`
 }
+
+type PublicKeyReq struct {
+	g.Meta `path:"/publickey" method:"get"`
+}
+type PublicKeyRes struct {
+	g.Meta `mime:"application/json"`
+	Key    string `json:"key"`
+	Id     string `json:"id"`
+}
